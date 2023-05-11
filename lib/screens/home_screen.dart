989006187fdf2a59144f714032903067fefc9420
h.young <cocotree221@gmail.com>
2023-05-11 17:55:47 +0900

    timer 재생버튼

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; //Undefined class Timer error /수동으로 패키지 불러옴
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int taotalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      taotalSeconds = taotalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+      //periodic 에서 첫번째는 시간, 두번째는 void 형태의 Timer를 인수로 받는 함수
+      //late 키워드는 지연초기화, 나중에 사용할때 초기화됨
+      //late timer는 onStartPressed()함수가 호출될 떄 초기화, Timer객체 생성
+      //Timer 객체는 주기적으로 inTick 함수 실행, onTick함수에서 상태업데이트
+      // late Timer timer는 작동중에 계속 실행, onTick 함수에서 상태 업데이트
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                '$taotalSeconds', //25:00
                 style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -33,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: () {},
+                onPressed: onStartPressed, //(){}
                 icon: const Icon(Icons.play_circle_outlined),
               ),
             ),
@@ -44,8 +66,16 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Expanded(
                   child: Container(
-                    decoration:
-                        BoxDecoration(color: Theme.of(context).cardColor),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      // borderRadius: BorderRadius.circular(50), 에뮬레이터 따라 ui보고 적용할것
+                      // borderRadius: const BorderRadius.only(
+                      //     topLeft: Radius.circular(50),
+                      //     topRight: Radius.circular(50)), //only 로 각각 주기
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(50),
+                      ),
+                    ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
